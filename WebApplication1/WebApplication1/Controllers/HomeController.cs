eaf32887017744ed80cd925454a1073b62e28f97
satishrabari <79073280+satishrabari@users.noreply.github.com>
2022-01-10 01:27:35 +0530

    project uploaded

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+
+            return View();
+        }
+        [Route("selfies")]
+        [HttpPost]
+        public async Task<IActionResult> selfiesAsync([FromForm] IFormCollection postData)
+        {
+            using var ms = new MemoryStream();
+            postData.Files["selfie"].CopyTo(ms);
+            var image = ms.ToArray();
+
+            await _db.SAMP.AddAsync(new SAMP
+            {
+                CreatedAt = DateTime.Now,
+                CreatedById = User.FindFirstValue(ClaimTypes.NameIdentifier),
+                Image = image,
+                Lat = 0,
+                Long = 0,
+                Location = postData["location"],
+                Title = postData["title"],
+                UniqueId = postData["id"]
+            });
+            await _db.SaveChangesAsync();
+            return Ok(new { id = postData["id"] });
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/SAMP.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Models
+{
+    public class SAMP
+    {
+        public int Id { get; set; }
+        public string? UniqueId { get; set; }
+        public string? Title { get; set; }
+        public byte[]? Image { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public string? Location { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? CreatedById { get; set; }
+    }
+
+}
